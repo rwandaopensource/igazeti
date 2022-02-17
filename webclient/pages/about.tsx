f24e1c763ca00d5f48cd93d34fb21607b8721651
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2022-02-18 00:00:27 +0200

    add tailwind css to the project

--- webclient/pages/about.tsx
+++ webclient/pages/about.tsx
@@ -1,9 +1,24 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from 'next';
 
-const About: NextPage = () => {
-    return (
-        <div>this is the about page</div>
-    )
+interface AboutPageProps {
+    data: {
+        name: string
+    }
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await fetch('http://localhost:3000/api/hello');
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const About: NextPage<AboutPageProps> = ({ data }) => {
+  return <div className="text-3xl font-bold underline">{data.name}</div>;
+};
 
 export default About;
